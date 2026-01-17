d143da7c3b45a757f485256417c4801cf11841e7
3250-omar <private.w556@gmail.com>
2026-01-17 06:32:21 +0200

    edit

--- app/hooks/useGetSocialMedia.ts
+++ app/hooks/useGetSocialMedia.ts
@@ -4,14 +4,26 @@ import { createClient } from "@/utils/supabase/client";
 import { useEffect, useState } from "react";
 
 export default function useGetSocialMedia() {
-  const [socialmedia, setSocialmedia] = useState<any[] | null>(null);
+  const [socialmedia, setSocialmedia] = useState<any[]>([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState<any>(null);
 
   useEffect(() => {
     const fetchSocialMedia = async () => {
+      console.log("Checking environment variables...");
+      const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
+      const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
+      console.log("SUPABASE_URL defined:", !!supabaseUrl);
+      console.log("SUPABASE_ANON_KEY defined:", !!supabaseKey);
+
+      console.log("Starting social media fetch...");
       try {
         const supabase = createClient();
+        if (!supabase) {
+          console.error("Supabase client could not be created.");
+          setLoading(false);
+          return;
+        }
         const { data, error } = await supabase.from("socialmedia").select();
 
         if (error) {
